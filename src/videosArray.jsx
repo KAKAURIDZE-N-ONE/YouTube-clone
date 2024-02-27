d5ac1a6c9238dc382ce77ead3fb4c58e377f82f1
KAKAURIDZE_N_ONE <g_kakauridze3@cu.edu.ge>
2024-02-28 02:46:58 +0400

    I change url

--- src/videosArray.jsx
+++ src/videosArray.jsx
@@ -2,228 +2,228 @@ export const videosArray = [
   [
     {
       videoImg:
-        'https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q',
+        "https://i.ytimg.com/vi/uYPbbksJxIg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2M_LSAVX5C1yeaypLoWxlz78y5Q",
       pageImg:
-        'https://yt3.ggpht.com/QSCDnr5KmYElRp-zYpdW-txkiRo-HYF6Ah1SvMF_7MlspmO1fwYOy7P1jnim8HqezI8IZZKcPQ=s88-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video12.mp4',
-      videoName: 'Oppenheimer (2023) - The World Will Remember This Day!',
-      pageName: 'Movieclips',
-      quantityOfViews: '17K',
-      uploadDate: '1 day ago',
+        "https://yt3.ggpht.com/QSCDnr5KmYElRp-zYpdW-txkiRo-HYF6Ah1SvMF_7MlspmO1fwYOy7P1jnim8HqezI8IZZKcPQ=s88-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video12.mp4",
+      videoName: "Oppenheimer (2023) - The World Will Remember This Day!",
+      pageName: "Movieclips",
+      quantityOfViews: "17K",
+      uploadDate: "1 day ago",
       id: 11,
-      subscribers: '60.3M',
+      subscribers: "60.3M",
       duration: 186,
     },
     {
       videoImg:
-        'https://i.ytimg.com/vi/_x9jYEYO9Bg/hqdefault.jpg?sqp=-oaymwE2COADEI4CSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB1AaAAuADigIMCAAQARhlIGUoZTAP&rs=AOn4CLBQB8ntHzZpfa-8wZxgYhmMBAtdDQ',
+        "https://i.ytimg.com/vi/_x9jYEYO9Bg/hqdefault.jpg?sqp=-oaymwE2COADEI4CSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB1AaAAuADigIMCAAQARhlIGUoZTAP&rs=AOn4CLBQB8ntHzZpfa-8wZxgYhmMBAtdDQ",
       pageImg:
-        'https://yt3.ggpht.com/ytc/AIf8zZRKY64uWRglxABzko411fiQIEPoFykBGXdTKrqrnQ=s88-c-k-c0x00ffffff-no-rj',
+        "https://yt3.ggpht.com/ytc/AIf8zZRKY64uWRglxABzko411fiQIEPoFykBGXdTKrqrnQ=s88-c-k-c0x00ffffff-no-rj",
 
-      videoName: 'Amr Diab - habibi ya nour el ein ( El Clone)',
-      pageName: 'Ynain Amorim',
-      quantityOfViews: '22M',
-      uploadDate: '7 years ago',
+      videoName: "Amr Diab - habibi ya nour el ein ( El Clone)",
+      pageName: "Ynain Amorim",
+      quantityOfViews: "22M",
+      uploadDate: "7 years ago",
       id: 8,
-      subscribers: '39.4K',
+      subscribers: "39.4K",
       duration: 259,
     },
 
     {
       videoImg:
-        'https://i.ytimg.com/vi/ZG-HKX5jmko/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOqAwJYWkpPfnTZUbusFO2JRmY1Q',
-      pageImg:
-        'https://yt3.ggpht.com/BF1Hu2595RKC7zYGV-y3VvVcBFgYZuVyTYJqykrGEs4Tt_DXfNyfv0LuTAOKClQ2uwHD7j2kxJI=s88-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video4.mp4',
-
-      videoName: 'Fast Five: Dream life HD CLIP',
-      pageName: 'Kamil Kazimierczak',
-      quantityOfViews: '1.5M',
-      uploadDate: '6 years ago',
-      id: 3,
-      duration: 231,
-    },
-    {
-      videoImg:
-        'https://i.ytimg.com/vi/JFr0FA7ehfA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC2SqeCE589Spi5RcGxVIcGN2HoIQ',
+        "https://i.ytimg.com/vi/JFr0FA7ehfA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC2SqeCE589Spi5RcGxVIcGN2HoIQ",
 
       pageImg:
-        'https://yt3.googleusercontent.com/ytc/AIf8zZR5j88xw3BvPeEPQAQpfzw7oHfYe12_Bifn9KO9Mg=s176-c-k-c0x00ffffff-no-rj-mo',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video5.mp4',
-      videoName: 'სუფრა კახეთში Ft @Octopusi',
-      pageName: 'საეჭვო კაცი',
-      quantityOfViews: '150K',
-      uploadDate: '3 months ago',
+        "https://yt3.googleusercontent.com/ytc/AIf8zZR5j88xw3BvPeEPQAQpfzw7oHfYe12_Bifn9KO9Mg=s176-c-k-c0x00ffffff-no-rj-mo",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video5.mp4",
+      videoName: "სუფრა კახეთში Ft @Octopusi",
+      pageName: "საეჭვო კაცი",
+      quantityOfViews: "150K",
+      uploadDate: "3 months ago",
       id: 4,
       duration: 1911,
     },
 
     {
       videoImg:
-        'https://i.ytimg.com/vi/QdBZY2fkU-0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBsUIlbe3vsIz8Q1dMp4Jd7EzlHWg',
+        "https://i.ytimg.com/vi/QdBZY2fkU-0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBsUIlbe3vsIz8Q1dMp4Jd7EzlHWg",
       pageImg:
-        'https://yt3.ggpht.com/r7mISTzNbcLem_plduc_SKMh9KUiP8CWnuhS7H4-CCErF4juXxkrK-5_4LOIMeYl-lXAXMUG=s88-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video6.mp4',
+        "https://yt3.ggpht.com/r7mISTzNbcLem_plduc_SKMh9KUiP8CWnuhS7H4-CCErF4juXxkrK-5_4LOIMeYl-lXAXMUG=s88-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video6.mp4",
 
-      videoName: 'Grand Theft Auto VI Trailer 1',
-      pageName: 'Rockstar Games',
-      quantityOfViews: '134M',
-      uploadDate: '4 days ago',
+      videoName: "Grand Theft Auto VI Trailer 1",
+      pageName: "Rockstar Games",
+      quantityOfViews: "134M",
+      uploadDate: "4 days ago",
       id: 5,
       duration: 90,
     },
 
     {
       videoImg:
-        'https://i.ytimg.com/vi/5qm8PH4xAss/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLASuyULMeWqQRoK_EfTLw4rAIlMKw',
+        "https://i.ytimg.com/vi/5qm8PH4xAss/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLASuyULMeWqQRoK_EfTLw4rAIlMKw",
       pageImg:
-        'https://yt3.ggpht.com/ytc/AIf8zZRpTBCU44Os8Gstnt-Ak8YFA4RVWdaKUvRjo5G9mD8=s88-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video7.mp4',
+        "https://yt3.ggpht.com/ytc/AIf8zZRpTBCU44Os8Gstnt-Ak8YFA4RVWdaKUvRjo5G9mD8=s88-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video7.mp4",
 
-      videoName: '50 Cent - In Da Club (Official Music Video)',
-      pageName: '50 Cent',
-      quantityOfViews: '1.9B',
-      uploadDate: '14 years ago',
+      videoName: "50 Cent - In Da Club (Official Music Video)",
+      pageName: "50 Cent",
+      quantityOfViews: "1.9B",
+      uploadDate: "14 years ago",
       id: 6,
       duration: 247,
     },
     {
       videoImg:
-        'https://i.ytimg.com/vi/iHuawH_RToQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGBN0EdFGV13TZqENmAHmfZu7csw',
+        "https://i.ytimg.com/vi/iHuawH_RToQ/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAGBN0EdFGV13TZqENmAHmfZu7csw",
       pageImg:
-        'https://yt3.ggpht.com/ytc/APkrFKZqHnNIlD1b7js93FBEoJavzfn8LVuEcHmWj8_SiQ=s68-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video8.mp4',
+        "https://yt3.ggpht.com/ytc/APkrFKZqHnNIlD1b7js93FBEoJavzfn8LVuEcHmWj8_SiQ=s68-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video8.mp4",
 
-      videoName: 'y yo voy, voy, voy | Alexa Demie',
-      pageName: 'i want a sugar daddy',
-      quantityOfViews: '14M',
-      uploadDate: '2 years ago',
+      videoName: "y yo voy, voy, voy | Alexa Demie",
+      pageName: "i want a sugar daddy",
+      quantityOfViews: "14M",
+      uploadDate: "2 years ago",
       id: 7,
       duration: 111,
     },
 
     {
       videoImg:
-        'https://i.ytimg.com/vi/pbYMReX2ci4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAxBv5eYeXryEMhPfyI8XRoVUdNkg',
+        "https://i.ytimg.com/vi/pbYMReX2ci4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAxBv5eYeXryEMhPfyI8XRoVUdNkg",
       pageImg:
-        'https://yt3.ggpht.com/rzIEYXjbRwKx_Ix_dHa0XdZh4pSTIf505n5DgdJxmzRKzJT36jqHM86Dmp2l-l7WpIpzcGInMo4=s68-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video10.mp4',
+        "https://yt3.ggpht.com/rzIEYXjbRwKx_Ix_dHa0XdZh4pSTIf505n5DgdJxmzRKzJT36jqHM86Dmp2l-l7WpIpzcGInMo4=s68-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video10.mp4",
 
-      videoName: 'The Fast and The Furious - Tokyo Drift: Welcome to Tokyo',
-      pageName: 'Binge Society',
-      quantityOfViews: '30M',
-      uploadDate: '3 years ago',
+      videoName: "The Fast and The Furious - Tokyo Drift: Welcome to Tokyo",
+      pageName: "Binge Society",
+      quantityOfViews: "30M",
+      uploadDate: "3 years ago",
       id: 9,
       duration: 311,
     },
     {
       videoImg:
-        'https://i.ytimg.com/vi/SKg5G2obfO8/hq2.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCA-TF7n3W5H0Y6TdXM1m23cxApLw',
+        "https://i.ytimg.com/vi/SKg5G2obfO8/hq2.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCA-TF7n3W5H0Y6TdXM1m23cxApLw",
       pageImg:
-        'https://yt3.ggpht.com/CfAgdXDALV5i9g4ynP7_eqQkKxFCIHxLEPoG9I_eRHa7gkerwiBUXVsw8_-DZcVGxpRFU0pp=s88-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video3.mp4',
+        "https://yt3.ggpht.com/CfAgdXDALV5i9g4ynP7_eqQkKxFCIHxLEPoG9I_eRHa7gkerwiBUXVsw8_-DZcVGxpRFU0pp=s88-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video3.mp4",
 
-      videoName: 'Gisele (Fast & Furious) - Gal Gadot - Danza Kuduro',
-      pageName: 'Almu Robbie',
-      quantityOfViews: '9.7M',
-      uploadDate: '5 years ago',
+      videoName: "Gisele (Fast & Furious) - Gal Gadot - Danza Kuduro",
+      pageName: "Almu Robbie",
+      quantityOfViews: "9.7M",
+      uploadDate: "5 years ago",
       id: 2,
       duration: 303,
     },
     {
       videoImg:
-        'https://i.ytimg.com/vi/pSY3i5XHHXo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDs4GstmNM4RwWWqiGZ7UyE-K5Qew',
+        "https://i.ytimg.com/vi/pSY3i5XHHXo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDs4GstmNM4RwWWqiGZ7UyE-K5Qew",
       pageImg:
-        'https://yt3.ggpht.com/jx6B6Jx83KSpFf5FFl9yf8DcyMHhpgLUSuhRD6s261WivhyFttBMj6rsNtHXRWWpXwtLJf2Jfw=s88-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video11.mp4',
+        "https://yt3.ggpht.com/jx6B6Jx83KSpFf5FFl9yf8DcyMHhpgLUSuhRD6s261WivhyFttBMj6rsNtHXRWWpXwtLJf2Jfw=s88-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video11.mp4",
 
-      videoName: 'Central Cee x Dave - Sprinter [Music Video]',
-      pageName: 'Central Cee',
-      quantityOfViews: '156M',
-      uploadDate: '6 months ago',
+      videoName: "Central Cee x Dave - Sprinter [Music Video]",
+      pageName: "Central Cee",
+      quantityOfViews: "156M",
+      uploadDate: "6 months ago",
       id: 10,
       duration: 229,
     },
 
     {
       videoImg:
-        'https://i.ytimg.com/vi/86g3-PM6K5E/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCSUhr6A9ghvucw7Mn6vEX_yg9jpg',
+        "https://i.ytimg.com/vi/86g3-PM6K5E/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCSUhr6A9ghvucw7Mn6vEX_yg9jpg",
       pageImg:
-        'https://yt3.ggpht.com/gMlDKFoY6aqvkrKwveXmiAmJMv9I3ovIvsvzGvOLORv-b8xqcsXKP6vdYGuxcq7VJVft0hCgKDo=s88-c-k-c0x00ffffff-no-rj',
+        "https://yt3.ggpht.com/gMlDKFoY6aqvkrKwveXmiAmJMv9I3ovIvsvzGvOLORv-b8xqcsXKP6vdYGuxcq7VJVft0hCgKDo=s88-c-k-c0x00ffffff-no-rj",
       videoName:
         "AZYR - save me to i need your lovin' transition (Boiler Room x Teletech 2023)",
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video2.mp4',
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video2.mp4",
 
-      pageName: 'Techno Place',
-      quantityOfViews: '86K',
-      uploadDate: '2 months ago',
+      pageName: "Techno Place",
+      quantityOfViews: "86K",
+      uploadDate: "2 months ago",
       id: 1,
       duration: 464,
     },
     {
       videoImg:
-        'https://i.ytimg.com/vi/pIaL0b44sK8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDMJZ4E32sFZY2RtFy5oki0e1UvOA',
+        "https://i.ytimg.com/vi/pIaL0b44sK8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDMJZ4E32sFZY2RtFy5oki0e1UvOA",
       pageImg:
-        'https://yt3.ggpht.com/YUcpvhXKV4qIk3VQm4tQW9k3OOreJM0CauJO47wf0D-r3qUYACYa7jhtO0iEsf0DOr83q6zkrw=s88-c-k-c0x00ffffff-no-rj',
-      videoUrl: '/src/features/VIDEOSMAIN/videos/video1.mp4',
+        "https://yt3.ggpht.com/YUcpvhXKV4qIk3VQm4tQW9k3OOreJM0CauJO47wf0D-r3qUYACYa7jhtO0iEsf0DOr83q6zkrw=s88-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video1.mp4",
       videoName:
-        'KVARA: რა უთხრა სპალეტიმ ხვიჩას? • პირველი სცენასთან • რეალზე ოცნება დასრულდა?',
-      pageName: 'Geo Team • ჯეო თიმი',
-      quantityOfViews: '110k',
-      uploadDate: '3 days ago',
+        "KVARA: რა უთხრა სპალეტიმ ხვიჩას? • პირველი სცენასთან • რეალზე ოცნება დასრულდა?",
+      pageName: "Geo Team • ჯეო თიმი",
+      quantityOfViews: "110k",
+      uploadDate: "3 days ago",
       id: 0,
       duration: 1176,
+    },
+    {
+      videoImg:
+        "https://i.ytimg.com/vi/ZG-HKX5jmko/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOqAwJYWkpPfnTZUbusFO2JRmY1Q",
+      pageImg:
+        "https://yt3.ggpht.com/BF1Hu2595RKC7zYGV-y3VvVcBFgYZuVyTYJqykrGEs4Tt_DXfNyfv0LuTAOKClQ2uwHD7j2kxJI=s88-c-k-c0x00ffffff-no-rj",
+      videoUrl: "/src/features/VIDEOSMAIN/videos/video4.mp4",
+
+      videoName: "Fast Five: Dream life HD CLIP",
+      pageName: "Kamil Kazimierczak",
+      quantityOfViews: "1.5M",
+      uploadDate: "6 years ago",
+      id: 3,
+      duration: 231,
     },
   ],
   [
     {
       shortImg:
-        'https://i.ytimg.com/vi/zPxQjuFoUBc/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLBEzhO00BE14m9Z5SslJjB4nfwttw',
-      heading: 'დეკემბერი #ღამისშოუსბენდი',
-      views: '19K views',
+        "https://i.ytimg.com/vi/zPxQjuFoUBc/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLBEzhO00BE14m9Z5SslJjB4nfwttw",
+      heading: "დეკემბერი #ღამისშოუსბენდი",
+      views: "19K views",
     },
     {
       shortImg:
-        'https://i.ytimg.com/vi/VAOXIR4REgo/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLB2YfH2FWD69c4v3V-Scy5XagjhRQ',
-      heading: 'Levan“Hulk”Saginashvili vs Konstantine“Georgian Bull”',
-      views: '338K views',
+        "https://i.ytimg.com/vi/VAOXIR4REgo/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLB2YfH2FWD69c4v3V-Scy5XagjhRQ",
+      heading: "Levan“Hulk”Saginashvili vs Konstantine“Georgian Bull”",
+      views: "338K views",
     },
     {
       shortImg:
-        'https://i.ytimg.com/vi/-ubcgJ_4CPE/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLCqjSrP7IuhlgNGap1zR_ayKyC3Ig',
-      heading: 'Salome Kapanadze და wupaka',
-      views: '114k views',
+        "https://i.ytimg.com/vi/-ubcgJ_4CPE/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLCqjSrP7IuhlgNGap1zR_ayKyC3Ig",
+      heading: "Salome Kapanadze და wupaka",
+      views: "114k views",
     },
     {
       shortImg:
-        'https://i.ytimg.com/vi/_gvT7B96q_s/oardefault.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLCqN7tCClXxf6GBDqpiSFOt7TfOlA',
-      heading: 'Peanut butter cookies 🍪',
-      views: '2M views',
+        "https://i.ytimg.com/vi/_gvT7B96q_s/oardefault.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLCqN7tCClXxf6GBDqpiSFOt7TfOlA",
+      heading: "Peanut butter cookies 🍪",
+      views: "2M views",
     },
     {
       shortImg:
-        'https://i.ytimg.com/vi/LZ4j5LnB9vY/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLC4teoTAZGZC6OPV_a8WbtLr0LQQw',
-      heading: 'Levan VS Aziz',
-      views: '203M views',
+        "https://i.ytimg.com/vi/LZ4j5LnB9vY/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLC4teoTAZGZC6OPV_a8WbtLr0LQQw",
+      heading: "Levan VS Aziz",
+      views: "203M views",
     },
     {
       shortImg:
-        'https://i.ytimg.com/vi/rE1SUCqRBp4/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLA61XG-GnUZ87a7c-IfUEs7mipoTQ',
-      heading: 'Her eyes 🖤| Robin Hood #shorts #love',
-      views: '8.3M views',
+        "https://i.ytimg.com/vi/rE1SUCqRBp4/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLA61XG-GnUZ87a7c-IfUEs7mipoTQ",
+      heading: "Her eyes 🖤| Robin Hood #shorts #love",
+      views: "8.3M views",
     },
     {
       shortImg:
-        'https://i.ytimg.com/vi/AybaCoAZdSY/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLAXzqbh9QPiSITAjuwrEVeO6BGzWQ',
+        "https://i.ytimg.com/vi/AybaCoAZdSY/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLAXzqbh9QPiSITAjuwrEVeO6BGzWQ",
       heading:
-        'Random shots of the beautiful people #streetphotographytips #streetphotography',
-      views: '33M views',
+        "Random shots of the beautiful people #streetphotographytips #streetphotography",
+      views: "33M views",
     },
     {
       shortImg:
-        'https://i.ytimg.com/vi/E5WjgMQ1Gew/hq720.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLB3wxGIqJYkyizh00DRYXEnQjh_LA',
+        "https://i.ytimg.com/vi/E5WjgMQ1Gew/hq720.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLB3wxGIqJYkyizh00DRYXEnQjh_LA",
       heading: "She🖤 or dilaraa💚or dilaraa's sister❤?😩🔥",
-      views: '2M views',
+      views: "2M views",
     },
   ],
 ];

7f51a206af067ade04bc7a3aece2713ea47fff50
KAKAURIDZE_N_ONE <g_kakauridze3@cu.edu.ge>
2024-08-03 21:36:46 +0400

    fixed bugs

--- src/features/MAINNAVBAR/MainNavBarComponents/MainNav.jsx
+++ src/features/MAINNAVBAR/MainNavBarComponents/MainNav.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from 'react';
-import GoogleSignIn from './GoogleSignIn';
-import styles from './MainNav.module.css';
-import MainNavMenuSvg from '../MainNavBarSvgs/MainNavMenuSvg';
-import MainNavYoutubeSvg from '../MainNavBarSvgs/MainNavYoutubeSvg';
-import SearchBar from './SearchBar';
-import SettingDots from '../MainNavBarSvgs/SettingDots';
-import ArrowBack from '../MainNavBarSvgs/ArrowBack';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateWindowWidth } from '../MainNavBarSlice';
-import { updateWindowHeight } from '../../SIDENAVBAR/SideNavBarSlice';
+import { useEffect } from "react";
+import GoogleSignIn from "./GoogleSignIn";
+import styles from "./MainNav.module.css";
+import MainNavMenuSvg from "../MainNavBarSvgs/MainNavMenuSvg";
+import MainNavYoutubeSvg from "../MainNavBarSvgs/MainNavYoutubeSvg";
+import SearchBar from "./SearchBar";
+import SettingDots from "../MainNavBarSvgs/SettingDots";
+import ArrowBack from "../MainNavBarSvgs/ArrowBack";
+import { useDispatch, useSelector } from "react-redux";
+import { updateWindowWidth } from "../MainNavBarSlice";
+import { updateWindowHeight } from "../../SIDENAVBAR/SideNavBarSlice";
 
 function MainNav() {
-  const MainNavBar = useSelector(store => store.MainNavBar);
-  const VideosMain = useSelector(store => store.VideosMain);
+  const MainNavBar = useSelector((store) => store.MainNavBar);
+  const VideosMain = useSelector((store) => store.VideosMain);
 
   const dispatch = useDispatch();
 
@@ -23,11 +23,11 @@ function MainNav() {
       dispatch(updateWindowWidth(window.innerWidth));
     }
     // Add event listener for window resize
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     // Clean up the event listener on component unmount
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, [dispatch]);
 
@@ -36,10 +36,10 @@ function MainNav() {
       dispatch(updateWindowHeight(window.innerHeight));
     }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, [dispatch]);
 
@@ -49,64 +49,63 @@ function MainNav() {
   };
 
   return (
-    <nav className={styles['main-nav-box']} style={MAINNAVBOXSTYLE}>
+    <nav className={styles["main-nav-box"]} style={MAINNAVBOXSTYLE}>
       {!(MainNavBar.windowWidth <= 680 && MainNavBar.searchInputIsFocused) ? (
-        <div className={styles['cont-1']}>
+        <div className={styles["cont-1"]}>
           <MainNavMenuSvg />
           <MainNavYoutubeSvg />
         </div>
       ) : (
         <ArrowBack />
       )}
-
       <div
-        className={styles['cont-2']}
+        className={styles["cont-2"]}
         style={
           MainNavBar.windowWidth <= 680 && MainNavBar.searchInputIsFocused
-            ? { marginLeft: 0, width: '99.98%', marginRight: '1rem' }
+            ? { marginLeft: 0, width: "99.98%", marginRight: "1rem" }
             : {}
         }
       >
         <SearchBar />
       </div>
       {!(MainNavBar.windowWidth <= 680 && MainNavBar.searchInputIsFocused) && (
-        <div className={styles['cont-3']}>
+        <div className={styles["cont-3"]}>
           {!VideosMain.videosIsLoading && <SettingDots />}
           {!VideosMain.videosIsLoading && <GoogleSignIn />}
           {VideosMain.videosIsLoading && (
             <div
               style={{
-                width: '24.1rem',
-                display: 'flex',
-                alignItems: 'center',
-                height: '2rem',
-                gap: '1rem',
+                width: "24.1rem",
+                display: "flex",
+                alignItems: "center",
+                height: "2rem",
+                gap: "1rem",
               }}
             >
               <div
                 style={{
-                  width: '3.2rem',
-                  height: '3.2rem',
-                  borderRadius: '50%',
-                  backgroundColor: 'rgba(255, 255, 255, 0.11)',
-                  marginLeft: 'auto',
+                  width: "3.2rem",
+                  height: "3.2rem",
+                  borderRadius: "50%",
+                  backgroundColor: "rgba(255, 255, 255, 0.11)",
+                  marginLeft: "auto",
                 }}
               ></div>
               <div
                 style={{
-                  width: '3.2rem',
-                  height: '3.2rem',
-                  borderRadius: '50%',
-                  backgroundColor: 'rgba(255, 255, 255, 0.11)',
+                  width: "3.2rem",
+                  height: "3.2rem",
+                  borderRadius: "50%",
+                  backgroundColor: "rgba(255, 255, 255, 0.11)",
                 }}
               ></div>
               <div
                 style={{
-                  width: '3.2rem',
-                  height: '3.2rem',
-                  borderRadius: '50%',
-                  backgroundColor: 'rgba(255, 255, 255, 0.11)',
-                  marginRight: '3rem',
+                  width: "3.2rem",
+                  height: "3.2rem",
+                  borderRadius: "50%",
+                  backgroundColor: "rgba(255, 255, 255, 0.11)",
+                  marginRight: "3rem",
                 }}
               ></div>
             </div>
--- src/features/SIDENAVBAR/SideNavBarComponents/MainSideNavBar.jsx
+++ src/features/SIDENAVBAR/SideNavBarComponents/MainSideNavBar.jsx
@@ -1,33 +1,33 @@
-import styles from './MainSideNavBar.module.css';
-import PropTypes from 'prop-types';
-import GoogleSignIn from '../../MAINNAVBAR/MainNavBarComponents/GoogleSignIn';
-import { menuSvgsArray } from '../menuSvgsArray';
-import { useDispatch, useSelector } from 'react-redux';
+import styles from "./MainSideNavBar.module.css";
+import PropTypes from "prop-types";
+import GoogleSignIn from "../../MAINNAVBAR/MainNavBarComponents/GoogleSignIn";
+import { menuSvgsArray } from "../menuSvgsArray";
+import { useDispatch, useSelector } from "react-redux";
 import {
   isHoveringSideNavBar,
   isNotHoveringSideNavBar,
   updateActiveMenuElement,
-} from '../SideNavBarSlice';
-import { NavLink } from 'react-router-dom';
+} from "../SideNavBarSlice";
+import { NavLink } from "react-router-dom";
 
 const footerWords = [
-  'About',
-  'Press',
-  'Copyright',
-  'Contact us',
-  'Creators',
-  'Advertise',
-  'Developers',
-  'Terms',
-  'Privacy',
-  'Policy & Safety',
-  'How YouTube works',
-  'Test new features',
+  "About",
+  "Press",
+  "Copyright",
+  "Contact us",
+  "Creators",
+  "Advertise",
+  "Developers",
+  "Terms",
+  "Privacy",
+  "Policy & Safety",
+  "How YouTube works",
+  "Test new features",
 ];
 
 function SideNavBar({ needNoPadding }) {
-  const MainNavBar = useSelector(store => store.MainNavBar);
-  const SideNavBar = useSelector(store => store.SideNavBar);
+  const MainNavBar = useSelector((store) => store.MainNavBar);
+  const SideNavBar = useSelector((store) => store.SideNavBar);
   const dispatch = useDispatch();
 
   function handleUpdateMenuActiveElement(el) {
@@ -43,22 +43,28 @@ function SideNavBar({ needNoPadding }) {
 
   const SIDENAVBARSTYLE = {
     height: `${SideNavBar.windowHeight - 55}px`,
-    overflow: SideNavBar.isHoveringSideNavBar ? 'auto' : 'hidden',
-    width: SideNavBar.isHoveringSideNavBar ? '23.3rem' : '22.5rem',
-    position: MainNavBar.windowWidth <= 1300 && 'sticky',
-    zIndex: '10',
+    overflow:
+      window.innerWidth > 480
+        ? SideNavBar.isHoveringSideNavBar
+          ? "auto"
+          : "hidden"
+        : "auto",
+    width: SideNavBar.isHoveringSideNavBar ? "23.3rem" : "22.5rem",
+    position: MainNavBar.windowWidth <= 1300 && "sticky",
+    zIndex: "10",
   };
 
+  // if (false)
   return (
     <div
-      className={styles['side-nav-bar']}
+      className={styles["side-nav-bar"]}
       style={SIDENAVBARSTYLE}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <div
-        className={styles['side-nav-bar-container']}
-        style={needNoPadding ? { paddingTop: '1rem' } : {}}
+        className={styles["side-nav-bar-container"]}
+        style={needNoPadding ? { paddingTop: "1rem" } : {}}
       >
         {menuSvgsArray.map((el, i) => {
           const isActiveSvg = el.name === SideNavBar.activeMenuElement;
@@ -67,21 +73,21 @@ function SideNavBar({ needNoPadding }) {
             return (
               <NavLink
                 to={el.url}
-                className={styles['side-nav-bar-block']}
+                className={styles["side-nav-bar-block"]}
                 style={{
-                  textDecoration: 'none',
-                  backgroundColor: isActiveSvg && 'rgba(255,255,255,0.085)',
+                  textDecoration: "none",
+                  backgroundColor: isActiveSvg && "rgba(255,255,255,0.085)",
                 }}
                 key={el.name}
                 onClick={() => handleUpdateMenuActiveElement(el.name)}
               >
                 {isActiveSvg ? el.activeSvg : el.svg}
-                <h1 style={{ fontWeight: isActiveSvg && '500' }}>{el.name}</h1>
+                <h1 style={{ fontWeight: isActiveSvg && "500" }}>{el.name}</h1>
               </NavLink>
             );
         })}
       </div>
-      <div className={styles['side-nav-bar-container']}>
+      <div className={styles["side-nav-bar-container"]}>
         {menuSvgsArray.map((el, i) => {
           const isActiveSvg = el.name === SideNavBar.activeMenuElement;
 
@@ -89,36 +95,36 @@ function SideNavBar({ needNoPadding }) {
             return (
               <NavLink
                 to={el.url}
-                className={styles['side-nav-bar-block']}
+                className={styles["side-nav-bar-block"]}
                 style={{
-                  textDecoration: 'none',
-                  backgroundColor: isActiveSvg && 'rgba(255,255,255,0.085)',
+                  textDecoration: "none",
+                  backgroundColor: isActiveSvg && "rgba(255,255,255,0.085)",
                 }}
                 key={el.name}
                 onClick={() => handleUpdateMenuActiveElement(el.name)}
               >
                 {isActiveSvg ? el.activeSvg : el.svg}
-                <h1 style={{ fontWeight: isActiveSvg && '500' }}>{el.name}</h1>
+                <h1 style={{ fontWeight: isActiveSvg && "500" }}>{el.name}</h1>
               </NavLink>
             );
         })}
       </div>
-      <div className={styles['side-nav-bar-container']}>
-        <h1 className={styles['sign-in-text']}>
+      <div className={styles["side-nav-bar-container"]}>
+        <h1 className={styles["sign-in-text"]}>
           Sign in to like videos, comment, and subscribe.
         </h1>
-        <div className={styles['sign-in-box']}>
+        <div className={styles["sign-in-box"]}>
           <GoogleSignIn />
         </div>
       </div>
-      <div className={styles['side-nav-bar-container']}>
-        <h1 className={styles['container-heading']}>Explore</h1>
+      <div className={styles["side-nav-bar-container"]}>
+        <h1 className={styles["container-heading"]}>Explore</h1>
         {menuSvgsArray.map((el, i) => {
           const isActiveSvg = el.name === SideNavBar.activeMenuElement;
           if (i <= 8 && i >= 5)
             return (
               <div
-                className={styles['side-nav-bar-block']}
+                className={styles["side-nav-bar-block"]}
                 key={el.name}
                 onClick={() => handleUpdateMenuActiveElement(el.name)}
               >
@@ -128,33 +134,33 @@ function SideNavBar({ needNoPadding }) {
             );
         })}
       </div>
-      <div className={styles['side-nav-bar-container']}>
+      <div className={styles["side-nav-bar-container"]}>
         <div
-          className={styles['side-nav-bar-block']}
+          className={styles["side-nav-bar-block"]}
           onClick={() => handleUpdateMenuActiveElement(menuSvgsArray[9].name)}
         >
           {menuSvgsArray[9].svg}
           <h1>{menuSvgsArray[9].name}</h1>
         </div>
       </div>
-      <div className={styles['side-nav-bar-container']}>
-        <h1 className={styles['container-heading']}>More from YouTube</h1>
+      <div className={styles["side-nav-bar-container"]}>
+        <h1 className={styles["container-heading"]}>More from YouTube</h1>
         <div
-          className={styles['side-nav-bar-block']}
+          className={styles["side-nav-bar-block"]}
           onClick={() => handleUpdateMenuActiveElement(menuSvgsArray[10].name)}
         >
           {menuSvgsArray[10].svg}
           <h1>{menuSvgsArray[10].name}</h1>
         </div>
       </div>
-      <div className={styles['side-nav-bar-container']}>
+      <div className={styles["side-nav-bar-container"]}>
         {menuSvgsArray.map((el, i) => {
           const isActiveSvg = el.name === SideNavBar.activeMenuElement;
 
           if (i <= 14 && i >= 11)
             return (
               <div
-                className={styles['side-nav-bar-block']}
+                className={styles["side-nav-bar-block"]}
                 key={el.name}
                 onClick={() => handleUpdateMenuActiveElement(el.name)}
               >
@@ -164,28 +170,28 @@ function SideNavBar({ needNoPadding }) {
             );
         })}
       </div>
-      <footer className={styles['footer-box']}>
-        <div className={styles['footer-component']}>
+      <footer className={styles["footer-box"]}>
+        <div className={styles["footer-component"]}>
           {footerWords.map(
             (el, i) =>
               i <= 6 && (
-                <h4 className={styles['footer-links']} key={i}>
+                <h4 className={styles["footer-links"]} key={i}>
                   {el}
                 </h4>
               )
           )}
         </div>
-        <div className={styles['footer-component']}>
+        <div className={styles["footer-component"]}>
           {footerWords.map(
             (el, i) =>
               i > 6 && (
-                <h4 className={styles['footer-links']} key={i}>
+                <h4 className={styles["footer-links"]} key={i}>
                   {el}
                 </h4>
               )
           )}
         </div>
-        <h3 className={styles['copyrighting']}>© 2023 Google LLC</h3>
+        <h3 className={styles["copyrighting"]}>© 2023 Google LLC</h3>
       </footer>
     </div>
   );
